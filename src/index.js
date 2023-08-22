65572d1120bdf17ed163392ed70fc9f4f7bccc2a
Ahmed Karara <110180686+Ahmed-Karara@users.noreply.github.com>
2023-08-23 00:04:18 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}>
-      </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
